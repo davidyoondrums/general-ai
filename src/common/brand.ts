dd6367ad9b9c92fa85547f1c67d8162351aef397
dyoon <11621570+davidyoondrums@users.noreply.github.com>
2023-05-13 20:21:44 -0400

    chore: customize to reflect general-ai name

--- src/common/brand.ts
+++ src/common/brand.ts
@@ -7,24 +7,24 @@
  *  - public/manifest.json  name, short_name, description, theme_color, background_color
  */
 export const Brand = {
-  // Name: 'big-AGI',
-  // UpperName: 'BIG-AGI',
+  // Name: 'General-AGI',
+  // UpperName: 'General-AGI',
   Title: {
     Common: (process.env.NODE_ENV === 'development' ? '[DEV] ' : '') + 'big-AGI',
   },
   Meta: {
-    SiteName: 'big-AGI',
-    Title: 'big-AGI: Personal AGI App',
-    Description: 'big-AGI is a free, open-source project to build a general artificial intelligence (AGI) that can solve any problem.',
+    SiteName: 'General-AI',
+    Title: 'General-AI: Personal AI App',
+    Description: 'General-AI is a free, open-source project to build a general artificial intelligence (AGI) that can solve any problem.',
     Keywords: 'artificial general intelligence, agi, openai, gpt-4, ai personas, code execution, pdf import, voice i/o, ai chat, artificial intelligence',
     ThemeColor: '#434356',
-    TwitterSite: '@bigagienergy',
+    TwitterSite: '@davidyoondrums',
   },
   URIs: {
     // Slug: 'big-agi',
-    Home: 'https://big-agi.com',
+    Home: 'https://yoon.ai',
     CardImage: 'https://big-agi.com/icons/card-dark-1200.png',
-    OpenRepo: 'https://github.com/enricoros/big-agi',
+    OpenRepo: 'https://github.com/davidyoondrums/general-ai',
     SupportInvite: 'https://discord.gg/MkH4qj2Jp9',
   },
 };
